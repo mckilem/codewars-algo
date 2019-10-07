39d2da692155af9dfd96f520b6a1bd36256d7bd4
mckilem <andrianovgd@gmail.com>
2019-10-07 15:05:15 +0500

    fixed tests

--- java/src/test/java/com/codewars/NextBiggerTest.java
+++ java/src/test/java/com/codewars/NextBiggerTest.java
@@ -7,10 +7,10 @@ import org.junit.runners.JUnit4;
 public class NextBiggerTest {
     @Test
     public void basicTests() {
-        assertEquals(21, NextBigger.nextBiggerNumber(12));
-        assertEquals(531, NextBigger.nextBiggerNumber(513));
-        assertEquals(2071, NextBigger.nextBiggerNumber(2017));
-        assertEquals(441, NextBigger.nextBiggerNumber(414));
-        assertEquals(414, NextBigger.nextBiggerNumber(144));
+        assertEquals(21, NextBiggerWithSameDigits.nextBiggerNumber(12));
+        assertEquals(531, NextBiggerWithSameDigits.nextBiggerNumber(513));
+        assertEquals(2071, NextBiggerWithSameDigits.nextBiggerNumber(2017));
+        assertEquals(441, NextBiggerWithSameDigits.nextBiggerNumber(414));
+        assertEquals(414, NextBiggerWithSameDigits.nextBiggerNumber(144));
     }
 }
--- java/src/test/java/com/codewars/NextSmallerTest.java
+++ java/src/test/java/com/codewars/NextSmallerTest.java
@@ -8,12 +8,12 @@ public class NextSmallerTest {
 
     @Test
     public void basicTests() {
-        assertEquals(12, NextSmaller.nextSmaller(21));
-        assertEquals(790, NextSmaller.nextSmaller(907));
-        assertEquals(513, NextSmaller.nextSmaller(531));
-        assertEquals(-1, NextSmaller.nextSmaller(1027));
-        assertEquals(414, NextSmaller.nextSmaller(441));
-        assertEquals(123456789, NextSmaller.nextSmaller(123456798));
-        assertEquals(1234567918, NextSmaller.nextSmaller(1234567981));
+        assertEquals(12, NextSmallerWithSameDigits.nextSmaller(21));
+        assertEquals(790, NextSmallerWithSameDigits.nextSmaller(907));
+        assertEquals(513, NextSmallerWithSameDigits.nextSmaller(531));
+        assertEquals(-1, NextSmallerWithSameDigits.nextSmaller(1027));
+        assertEquals(414, NextSmallerWithSameDigits.nextSmaller(441));
+        assertEquals(123456789, NextSmallerWithSameDigits.nextSmaller(123456798));
+        assertEquals(1234567918, NextSmallerWithSameDigits.nextSmaller(1234567981));
     }
 }
